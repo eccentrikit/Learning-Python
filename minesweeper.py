128c7e833933933d613cefa78cc6e809c6a19023
eccentrikit <eccentrikit@users.noreply.github.com>
2022-05-05 16:58:01 -0700

    Improvements?
    Probably broke things idk anymore, sleepy... Zzz

--- minesweeper.py
+++ minesweeper.py
@@ -15,15 +15,17 @@ class Minesweeper:
       self.columns = 16
       self.mines = 40
     elif difficulty == 3:
-      self.rows = 30
-      self.columns = 16
+      self.rows = 16
+      self.columns = 30
       self.mines = 99
       
   def generateBoard(self):
-    self.mineBoard = [[0 for _ in range(self.rows)] for _ in range(self.columns)]
+    self.mineBoard = [[0 for _ in range(self.columns)] for _ in range(self.rows)]
     for i in range(self.mines):
       x, y = self.createBomb()
+      print("C", x, y)
       for sx, sy in self.getValidSpaces(x, y):
+        print("B", sx, sy)
         if self.mineBoard[sy][sx] != "X":
           self.mineBoard[sy][sx] += 1
 
@@ -37,20 +39,20 @@ class Minesweeper:
       return self.createBomb()
 
   def generatePlayerBoard(self):
-    self.playerBoard = [["-" for _ in range(self.rows)] for _ in range(self.columns)]
+    self.playerBoard = [["-" for _ in range(self.columns)] for _ in range(self.rows)]
     
   def displayBoard(self, board):
-      print("  " + " ".join(str(i + 1) for i in range(self.rows)))
-      print("  " + "="*self.columns*2)
-      for idx, row in enumerate(board):
-        print(str(idx + 1) + "|" + " ".join(str(cell) for cell in row))
-        print("")
+    print("  " + "".join((" " if i < 9 else "") + str(i + 1) for i in range(self.columns)))
+    print("  " + "="*self.columns*2)
+    for idx, row in enumerate(board):
+      print((" " if idx < 9 else "") + str(idx + 1) + "|" + " ".join(str(cell) for cell in row))
+      print("")
 
   def getValidSpaces(self, x, y):
     spaces = [(x + 1, y), (x + 1, y + 1), (x, y + 1), (x - 1, y + 1),
                 (x - 1, y), (x - 1, y - 1), (x, y - 1), (x + 1, y - 1)]
-    return list(filter(lambda s: s[0] >= 0 and s[0] <= self.rows - 1 
-                    and s[1] >= 0 and s[1] <= self.columns - 1, spaces))
+    return list(filter(lambda s: s[0] >= 0 and s[0] <= self.columns - 1 
+                    and s[1] >= 0 and s[1] <= self.rows - 1, spaces))
       
   def checkSpace(self, x, y):
     if (self.mineBoard[y][x] == 0):
@@ -79,14 +81,20 @@ class Minesweeper:
         coords = input(f"Enter coordinates ({self.rows} {self.columns}): ")
         coords = coords.split(" ")
         if coords[0].lower() == "f":
-          x, y = list(map(lambda c: int(c) - 1, coords[1:))
+          x, y = list(map(lambda c: int(c) - 1, coords[1:]))
+          if x < 0 or y < 0 or self.playerBoard[y][x] not in ("-", "F"):
+            continue
           self.flagSpace(x, y)
         else:
           x, y = list(map(lambda c: int(c) - 1, coords))
+          if x < 0 or y < 0:
+            continue
           if (self.mineBoard[y][x] == "X"):
             print("Game Over!")
             self.running = False
             self.playerBoard = self.mineBoard
+          elif (self.playerBoard[y][x] != "-"):
+            continue
           else:
             self.checkSpace(x, y)
         self.displayBoard(self.playerBoard)
@@ -94,8 +102,16 @@ class Minesweeper:
       except:
         pass
 
-  def start(self, difficulty):
-    self.setDifficulty(1)
+  def start(self):
+    while True:
+      try:
+        difficulty = int(input("Difficulty (1,2,3): "))
+        if 0 < difficulty < 4:
+          break
+      except:
+        pass
+        
+    self.setDifficulty(difficulty)
     self.generateBoard()
     self.generatePlayerBoard()
     self.displayBoard(self.mineBoard)
@@ -111,4 +127,4 @@ class Minesweeper:
       print("Score: ", score)
 
 game = Minesweeper()
-game.start(1)
+game.start()
